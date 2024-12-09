cf36ac5302e16ac09f02797a8768f98a47402584
spidweb2000 <spidweb2000@gmail.com>
2024-12-09 16:02:43 +0800

    changes name, add js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// Toggle navigation menu on click for mobile
 function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 }
+
+// Toggle dropdown menu on mobile
+document.querySelectorAll('.dropdown').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
